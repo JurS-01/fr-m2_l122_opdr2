4092ea6f48a9fc77f55121094255125e34ea55d3
Jurian Sapoetra <juriansapoetra@gmail.com>
2020-09-18 15:15:41 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const makeHomework = (course, callback) => {
+    setTimeout(() => {
+        console.log(`I will do my ${course} homework now`);
+        callback();
+    }, 2000);
+}
+
+const homeworkFinished = () => {
+    console.log("Look Mommy, I finished my homework");
+}
+
+makeHomework('math', homeworkFinished);
+// makeHomework('math');
+// homeworkFinished();
